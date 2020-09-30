bdfadf00d5618af82d4d82be369594301ab52f68
cleminator <alvinclemens@gmail.com>
2020-09-30 17:13:36 -0600

    working on the styling

--- java.js
+++ java.js
@@ -31,6 +31,8 @@ function start(city) {
 
   $.ajax(
     `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`
+  
+    // old api call replaced with the above api call
     // "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
   )
     // We store all of the retrieved data inside of an object called "response"
@@ -40,91 +42,94 @@ function start(city) {
       // // Log the resulting object
       console.log(response);
 
-      // // // Transfer content to HTML
-
-      // // console.log(currentlong);
-      // $("#ws").text("Wind Speed: " + response.list[0].wind.speed);
-      // console.log(response.wind.speed)
-      // $("#hum").text("Humidity: " + response.main.humidity);
-
-      // // // Convert the temp to fahrenheit
+     
+      
       
       
       var fahrenheit = toFahrenheit(response.list[0].main.feels_like) ;
-
-      // // // add temp content to html
-      // $("#temp").text("Temperature (F) " + fahrenheit.toFixed(2));
-      // $("#city1").text(response.name);
+      
       // console.log()
-
-      // let currentlat = response.coord.lat;
-      // let currentlong = response.coord.lon;
+      
       // console.log(currentlat)
       // console.log(currentlong)
-     
-      function fiveday(dayweather, listhour){
-      let weather = response.list[listhour]
       
+      // add temp content to html
+      function fiveday(dayweather, listhour){
+        let weatherbox = response.list[listhour]
+        
         // $(".dayweather" + dayweather).text("5 day forcast: " + toFahrenheit(weather.main.feels_like))
         // $(".dayweather" + dayweather).text("5 day forcast: " (weather.main.wind.))
+        
         $(".dayweather" + dayweather).html(`
-        <p>${toFahrenheit(weather.main.feels_like)}</p>
-        <p>${weather.main.wind}</p>
+        
+        <p class"text-center"> ${weatherbox.weather[0].description}</p>
+        
+        <p>Temperature: ${toFahrenheit(weatherbox.main.feels_like)} Degrees Fahrenheit</p>
+        
+        <p>Wind speed: ${weatherbox.wind.speed} Mph</p>
+        
         `)
-      
+        
+        $("#ws").text("Wind Speed: " + weatherbox.wind.speed);        
+        $("#hum").text("Humidity: " + weatherbox.main.humidity);
+        $("#city1").text(response.city.name);
+        $("#temp").text("Temperature: " + toFahrenheit(weatherbox.main.feels_like))
         
       }
-     fiveday(1, 4)
-     fiveday(2, 12)
-     fiveday(3, 20)
-     fiveday(4, 28)
-     fiveday(5, 36)
-     
+      fiveday(1, 4)
+      fiveday(2, 12)
+      fiveday(3, 20)
+      fiveday(4, 28)
+      fiveday(5, 36)
+      
+      let currentlat = response.city.coord.lat;
+      let currentlong = response.city.coord.lon;
       $.ajax(
         "http://api.openweathermap.org/data/2.5/uvi?&lat=" + currentlat + "&appid=" + APIKey +"&lon=" + currentlong
-      )
+        )
         .then(function (response) {
           console.log(response)
-          $("#uv").text("Wind Speed: " + response.value);
+          $("#uv").text("UV index: " + response.value);
         })
-
-    });
-
-
-}
-
-
-// // Log the data in the console as well
-// console.log("Wind Speed: " + response.wind.speed);
-// console.log("Humidity: " + response.main.humidity);
-// console.log("Temperature (F): " + fahrenheit);  
-
-
-
-
-
-
-
-$("#add-city").on("click", function (event) {
-  event.preventDefault();
-  // start('lima')
-  // start("burlington")
-  // start('radnor')
-  // // cityName = $("")
-  // console.log("#add-city")
-
-  let city3 = $("#search").val()
-  start(city3)
-})
-
-
-
-function toFahrenheit(kelvin){
-
-  return ((kelvin - 273.15) * 1.80 + 32).toFixed(0)
-   
-}
-// appid={appid}&lat={lat}&lon={lon}
+        
+      });
+    }
+      
+      
+    
+    
+    // // Log the data in the console as well
+    // console.log("Wind Speed: " + response.wind.speed);
+    // console.log("Humidity: " + response.main.humidity);
+    // console.log("Temperature (F): " + fahrenheit);  
+    
+    
+    
+    
+    
+    
+    
+    $("#add-city").on("click", function (event) {
+      event.preventDefault();
+      // start('lima')
+      // start("burlington")
+      // start('radnor')
+      // // cityName = $("")
+      // console.log("#add-city")
+      
+      let city3 = $("#search").val()
+      start(city3)
+    })
+    
+    
+    
+    // // // Convert the temp to fahrenheit
+    function toFahrenheit(kelvin){
+      
+      return ((kelvin - 273.15) * 1.80 + 32).toFixed(0)
+      
+    }
+    // appid={appid}&lat={lat}&lon={lon}
 
 
 
