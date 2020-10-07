91fd9ada0fc3b4166446651125164f336e87c7a2
cleminator <alvinclemens@gmail.com>
2020-10-07 10:45:33 -0600

    changed background

--- java.js
+++ java.js
@@ -111,11 +111,7 @@ function start(city) {
     
     $("#add-city").on("click", function (event) {
       event.preventDefault();
-      // start('lima')
-      // start("burlington")
-      // start('radnor')
-      // // cityName = $("")
-      // console.log("#add-city")
+    
       
       let city3 = $("#search").val()
       start(city3)
@@ -129,106 +125,11 @@ function start(city) {
       return ((kelvin - 273.15) * 1.80 + 32).toFixed(0)
       
     }
-    // appid={appid}&lat={lat}&lon={lon}
 
 
 
 
 
-
-
-
-
-
-
-      // function wawa(thiswillsave, funbox, keyName) {
-//   $(thiswillsave).on("click", function () {
-//       // get text from wawa 
-//       var textarea = $(funbox).val();
-//       // text area is saved in local storage
-//       localStorage.setItem(keyName, textarea)
-//   })
-//   // lets turn this data into a btn
-//   var save = localStorage.getItem(keyName)
-//   $(funbox).val(save);
-
-// var storedValue = localStorage.getItem(keyName);
-//   if(storedValue){
-
-//   document.getElementById("yourcity").value;
-
-//   }
-//   var storedButton = localStorage.getItem("button");
-//   if(storedButton){
-
-//   // document.getElementsByClass("blauw").value = storedButton;
-
-//   }
-// }// wawa is pulling text/saveBtn activates/user-... is targeted for data... insertion
-// wawa("#add-city", ".form-control", "data1")
-// wawa("#add-city", ".form-control", "data2")
-// wawa("#add-city", ".form-control", "data3")
-// wawa("#add-city", ".form-control", "data4")
-// wawa("#add-city", ".form-control", "data5")
-// wawa("#add-city", ".form-control", "data6")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let citys = ["Salt lake city"];
-
-// function renderButtons() {
-
-//   // Deleting the movie buttons prior to adding new movie buttons
-//   // (this is necessary otherwise we will have repeat buttons)
-//   $("#yourcity").empty();
-
-//   // Looping through the array of movies
-//   for (var i = 0; i < citys.length; i++) {
-
-//     // Then dynamicaly generating buttons for each movie in the array.
-//     // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-//     var a = $("<button>");
-//     // Adding a class
-//     a.addClass("city");
-//     // Adding a data-attribute with a value of the movie at index i
-//     a.attr("data-name", citys[i]);
-//     // Providing the button's text with a value of the movie at index i
-//     a.text(citys[i]);
-//     // Adding the button to the HTML
-//     $("#yourcity").append(a);
-//   }
-// }
-
-// // This function handles events where one button is clicked
-// $("#add-city").on("click", function(event) {
-//   // event.preventDefault() prevents the form from trying to submit itself.
-//   // We're using a form so that the user can hit enter instead of clicking the button if they want
-//   event.preventDefault();
-
-//   // This line will grab the text from the input box
-//   let newcity = $("#search").val().trim();
-//   // The movie from the textbox is then added to our array
-//   citys.push(newcity);
-
-//   // calling renderButtons which handles the processing of our movie array
-//   renderButtons();
-// });
-
-// // Calling the renderButtons function at least once to display the initial list of movies
-// renderButtons();
 
 
 
